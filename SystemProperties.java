613a647d00d4447fa1fbfb3010cb7ff6c09675bd
Abarna16 <47447958+Abarna16@users.noreply.github.com>
2019-06-20 11:59:06 +0530

    Add files via upload

---
+++ SystemProperties.java
@@ -0,0 +1,9 @@
+package Practice;
+import java.util.Properties;
+public class SystemProperties {
+    public static void main(String args[])
+    {
+        Properties property=System.getProperties();
+        property.list(System.out);
+    }
+}
