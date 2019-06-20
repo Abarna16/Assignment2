f638c9a662adf1cbf1ce9b7a9254744dd5d5ad84
Abarna16 <47447958+Abarna16@users.noreply.github.com>
2019-06-20 13:13:22 +0530

    Add files via upload

---
+++ Access.java
@@ -0,0 +1,19 @@
+package Three;
+import Practice.*;
+
+//derived class && other pakage 
+public class Access extends AccessSpecifier{
+    public static void main(String args[])
+    {
+        Access a=new Access();
+        a.show();
+    }
+    void show()
+    {
+       
+    //System.out.println("private variable"+a);
+         System.out.println("protected variable "+b);
+          System.out.println("public variable "+c);
+       // System.out.println("default variable"+d); 
+    }
+}
